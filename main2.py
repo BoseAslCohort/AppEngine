a6c2fcaa423cd0b56b47b2b735a8095764d2a5ab
Marko-Stamenovic-Bose <marko_stamenovic@bose.com>
2017-08-02 19:16:12 -0700

    added nonworking /random

--- main2.py
+++ main2.py
@@ -20,10 +20,11 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import tensorflow as tf
 import numpy as np
-from IPython.display import YouTubeVideo
 import base64
 from subprocess import check_output
-
+import requests
+import google.datalab as datalab
+import os
 
 app = Flask(__name__)
 
@@ -50,6 +51,67 @@ def videos(vid):
     """)
 
     return vidtemplate.substitute(youtube_id=vid)
+
+@app.route('/random/')
+def random():
+    model_name = 'test_Moe'
+    model_version = 'v1'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']='qwiklabs-gcp-dbd8214a842cac50-317227781517.json'
+    api = 'https://ml.googleapis.com/v1/projects/{project}/models/{model}/versions/{version}:predict'
+    url = api.format(project=datalab.Context.default().project_id,
+                 model=model_name,
+                 version=model_version)
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + datalab.Context.default().credentials.get_access_token().access_token
+    }
+
+
+    vidtemplate = Template("""
+      <h2>
+        YouTube video link:
+        <a href="https://www.youtube.com/watch?v=${youtube_id}">
+          ${youtube_id}
+        </a>
+      </h2>
+
+      <iframe src="https://www.youtube.com/embed/${youtube_id}" width="853" height="480" frameborder="0" allowfullscreen></iframe>
+    """)
+
+    video_lvl_record = "./video_data/testa0.tfrecord"
+    # frame_lvl_record = "./frame_data/traina0.tfrecord"
+
+    vid_ids = []
+    labels = []
+    mean_rgb = []
+    mean_audio = []
+
+    # for example in tf.python_io.tf_record_iterator(video_lvl_record):
+    #     tf_example = tf.train.Example.FromString(example)
+    #     vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    #     labels.append(tf_example.features.feature['labels'].int64_list.value)
+    #     mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    #     mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+
+    example=next(tf.python_io.tf_record_iterator(video_lvl_record))
+    example = base64.b64encode(example)
+
+    # in order to do inference in the cloud you have to do a base64
+
+    ######
+    body = {
+      'instances': [{"b64": example}
+      ]
+    }
+    import ipdb; ipdb.set_trace()
+
+    response = requests.post(url, json=body, headers=headers)
+    video_id = response.json()['predictions'][0]['video_id']
+    predictions = response.json()['predictions'][0]['predictions']
+    class_indeces = response.json()['predictions'][0]['class_indexes']
+
+    return vidtemplate.substitute(youtube_id=video_id)
+
 
 @app.errorhandler(500)
 def server_error(e):
